99bdfdcd9b4191346ef9e64a46c93839b946023a
Archana Sankaranarayanan <asankar@netflix.com>
2017-07-21 15:21:37 -0700

    Added support for chained selectors

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ module.exports = postcss.plugin('postcss-scopeit', function (opts) {
 
     return function (css) {
         css.walkRules(function (rule) {
-            if (rule.selector.match(/^body.*$/)) {
-                rule.selector = rule.selector
-                    .replace(/^body/, `body.${scopeName}`);
-            } else {
-                rule.selector =
-                    `.${scopeName} ${rule.selector}`;
+
+            if (rule.selector !== ':root') {
+                var allSelectors = rule.selector.split(',');
+                var modifiedSelectors = [];
+                allSelectors.forEach(function(selector) {
+                    if (selector.match(/^body.*$/)) {
+                        modifiedSelectors.push(selector.replace(/^body/, `body.${scopeName}`));
+                    } else {
+                        modifiedSelectors.push(`.${scopeName} ${selector}`);
+                    }
+                });
+                rule.selector = modifiedSelectors.join(',')
             }
         });
     };
--- index.test.js
+++ index.test.js
@@ -15,3 +15,21 @@ it('scopes selectors correctly when not attached to body', () => {
     });
 });
 
+it('attaches scope to body directly', () => {
+    return run('body *{color:red;}', 'body.lbmt *{color:red;}', {
+        scopeName: 'lbmt'
+    });
+});
+
+it('does not attach to :root', () => {
+    return run(':root{--color-primary:blue;}', ':root{--color-primary:blue;}', {
+        scopeName: 'lbmt'
+    });
+});
+
+it('scopes selectors that are chained', () => {
+    return run('h1,h2,h3,h4,h5{color:red;}', '.lbmt h1,.lbmt h2,.lbmt h3,.lbmt h4,.lbmt h5{color:red;}', {
+        scopeName: 'lbmt'
+    });
+});
+
